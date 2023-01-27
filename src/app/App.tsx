6285744a1ee059efe2054fb97398abc3cdeaabcc
Sébastien VIAL <sebvi66@hotmail.fr>
2023-01-27 18:14:43 +0100

    feat: fetching json

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,26 @@
-import { MapContainer, TileLayer, Polyline, Circle, CircleMarker, Polygon, Rectangle } from "react-leaflet"
+import { MapContainer, TileLayer, Circle } from "react-leaflet"
 import L from "leaflet"
+import { useEffect, useState } from "react"
+
+interface PartFields {
+  status: number,
+  items: [Field]
+}
+
+interface Field {
+  // I found there was something called a GeoJSON while investigating, never heard of this
+  // but the boundaries looks like some.
+  boundaries: any
+}
 
 export default function App() {
+    const [json, setJson] = useState<PartFields>()
+    useEffect(() => {
+        fetch(`${process.env.PUBLIC_URL}/1-partfields.json`)
+            .then(r => r.json())
+            .then(setJson)
+    }, [json]);
+
     const center = L.latLng([51.505, -0.09])
     const fillBlueOptions = { fillColor: 'blue' }
 
@@ -11,6 +30,7 @@ export default function App() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        
         <Circle center={center} pathOptions={fillBlueOptions} radius={200} />
       </MapContainer>
     )
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,7 +3,7 @@ import logo from "../2-logo.png"
 export default function Logo(props: React.PropsWithChildren<{}>) {
     return (
         <>
-        <img className="fixed w-32 top-4 left-4 z-50" src={logo} />
+        <img className="fixed w-32 top-4 left-4 z-50 bg-white p-4 rounded-lg" src={logo} />
         {props.children}
         </>
     )
